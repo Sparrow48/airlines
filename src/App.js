1d6eb338646b70140038f93c6d7ce1cc8ac62121
Sparrow48 <abunasib48@gmail.com>
2022-04-16 01:01:16 +0600

    add home and details page component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./component/HomePage";
+import AirlinesDetails from "./component/AirlinesDetails";
+
 function App() {
-  return <div>Airlines</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/airlinesDetails" element={<AirlinesDetails />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
